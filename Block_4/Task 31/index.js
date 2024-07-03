5f4c2211b79ea8477dbbccb5cf10e859f1f4b8ed
Margarita <rita.ianut@gmail.com>
2024-07-03 21:20:00 +0300

    32

---
+++ Block_4/Task 31/index.js
@@ -0,0 +1,8 @@
+// function rayngosling ([arr]) { //arr - это массив
+// return arr.slice
+// }; это мое неверное решение
+
+//вот верное
+function rayngosling(arr) {
+    return arr.length
+};
---
+++ Block_4/Task 32/index.js
@@ -0,0 +1,3 @@
+function rayngosling (el) {
+    return el[0] // 'el[0]' - обращение к первому элементу массива.
+}
