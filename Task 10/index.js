251ed8cc92b523a308966598113ab1944da523f7
Margarita <rita.ianut@gmail.com>
2024-07-03 15:54:35 +0300

    Task10

---
+++ Task 10/index.js
@@ -0,0 +1,12 @@
+function ryangosling(param1) {
+  return String(param1);
+}
+ryangosling(10)
+
+function ryangoslingOne(param2) {
+  return Number(param2);
+}
+
+function ryangoslingTwo(param3) {
+  return Boolean(param3);
+}
